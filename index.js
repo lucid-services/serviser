bfedca4805e834509bbf6ce282f8b65300df55d0
fogine <7884288+fogine@users.noreply.github.com>
2016-09-27 18:36:38 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.Express     = require('express');
+exports.AppManager  = require('./lib/appManager.js');
+exports.App         = require('./lib/app.js');
+exports.Router      = require('./lib/router.js');
+exports.Route       = require('./lib/route.js');
+exports.RequestType = require('./lib/requestType.js');
