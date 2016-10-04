e24fe15edeb664ffc36ea8a98da099d6e9f4784d
fogine <7884288+fogine@users.noreply.github.com>
2016-10-04 16:12:59 +0200

    export module files which are part of public api, plus remove passport middleware which will be moved to auth service module only

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ exports.App         = require('./lib/app.js');
 exports.Router      = require('./lib/router.js');
 exports.Route       = require('./lib/route.js');
 exports.RequestType = require('./lib/requestType.js');
+exports.modelLoader = require('./lib/modelLoader.js');
+exports.database    = {
+    CouchbaseCluster: require('./lib/database/couchbase.js'),
+    sequelizeBuilder: require('./lib/database/sequelize.js')
+};
+exports.error       = {
+    IncompleteDataError: require('./lib/error/incompleteDataError.js'),
+    RequestError: require('./lib/error/requestError.js'),
+    RouteError: require('./lib/error/routeError.js'),
+    ServiceError: require('./lib/error/serviceError.js'),
+    UnauthorizedError: require('./lib/error/unauthorizedError.js')
+};
