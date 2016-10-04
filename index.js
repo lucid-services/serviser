ec9ee6d692cbaa14a45e364b5abff249912981ce
fogine <7884288+fogine@users.noreply.github.com>
2016-10-04 17:20:00 +0200

    export couchbaseODM module

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-exports.Express     = require('express');
-exports.AppManager  = require('./lib/appManager.js');
-exports.App         = require('./lib/app.js');
-exports.Router      = require('./lib/router.js');
-exports.Route       = require('./lib/route.js');
-exports.RequestType = require('./lib/requestType.js');
-exports.modelLoader = require('./lib/modelLoader.js');
+exports.Express      = require('express');
+exports.AppManager   = require('./lib/appManager.js');
+exports.CouchbaseODM = require('kouchbase-odm');
+exports.App          = require('./lib/app.js');
+exports.Router       = require('./lib/router.js');
+exports.Route        = require('./lib/route.js');
+exports.RequestType  = require('./lib/requestType.js');
+exports.modelLoader  = require('./lib/modelLoader.js');
 exports.database    = {
     CouchbaseCluster: require('./lib/database/couchbase.js'),
     sequelizeBuilder: require('./lib/database/sequelize.js')
