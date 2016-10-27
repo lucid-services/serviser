0497ac57d8d4a556fa6aad8da6e4dc5a9329db9e
fogine <7884288+fogine@users.noreply.github.com>
2016-10-27 13:12:53 +0200

    requestIdentity middleware tests

---
+++ tests/unit/middleware/requestIdentity.js
@@ -0,0 +1,41 @@
+var sinon     = require('sinon');
+var chai      = require('chai');
+var sinonChai = require("sinon-chai");
+var logger    = require('bi-logger');
+
+var requestIdentity = require('../../../lib/middleware/requestIdentity.js');
+var AppManager      = require('../../../lib/express/appManager.js');
+var Config          = require('../mocks/config.js');
+
+var expect = chai.expect;
+
+chai.use(sinonChai);
+chai.should();
+
+describe('requestIdentity middleware', function() {
+
+    before(function() {
+        this.models = {};
+        this.config = new Config();
+
+        this.appManager = new AppManager(this.config, this.models);
+        var app = this.app = this.appManager.buildApp();
+
+        this.res = {};
+        this.req = {};
+        this.next = sinon.spy();
+
+    });
+
+    beforeEach(function() {
+        this.next.reset();
+    });
+
+    it('should set `UID` property on the `req` object', function() {
+        requestIdentity.call(this.app, this.req, this.res, this.next);
+
+        this.req.UID.should.be.a('number');
+
+        this.next.should.have.been.calledOnce;
+    });
+});
