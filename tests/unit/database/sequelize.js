53a113d9a86ccb11d4adc2366ea3ef130c0866c1
fogine <7884288+fogine@users.noreply.github.com>
2016-11-01 13:23:23 +0100

    seuqlize builder - bugfixes + tests

--- tests/unit/database/sequelize.js
+++ tests/unit/database/sequelize.js
@@ -23,6 +23,31 @@ describe('sequelizeBuilder', function() {
         sequelize.should.be.an.instanceof(Sequelize);
     });
 
+    it('should build new Sequelize object with provided options', function() {
+        var options = {
+            host: 'localhost',
+            db: 'test',
+            username: 'root',
+            password: 'test',
+            dialect: 'postgres',
+            pool: {
+                min: 10,
+                max: 100,
+                idle: 10
+            },
+            ssl: true
+        };
+
+        var sequelize = sequelizeBuilder(options);
+
+        sequelize.config.should.have.property('database', options.db);
+        sequelize.config.should.have.property('username', options.username);
+        sequelize.config.should.have.property('password', options.password);
+        sequelize.config.should.have.property('host', options.host);
+        sequelize.config.should.have.property('pool').that.is.eql(options.pool);
+        sequelize.config.should.have.deep.property('dialectOptions.ssl', options.ssl);
+    });
+
     it('should add cache support to the created sequelize object', function() {
         var spy = sinon.spy(penetrator, 'penetrateCacheSupport');
 
@@ -52,5 +77,46 @@ describe('sequelizeBuilder', function() {
 
         spy.should.have.callCount(0);
         spy.restore();
+    });
+
+    it('should allow to define `classMethods` options', function() {
+
+        var classMethods = {
+            method1: function() {},
+            method2: function() {},
+            search: function() {},
+        };
+
+        var sequelize = sequelizeBuilder({
+            host: 'localhost',
+            db: 'test',
+            username: 'root',
+            dialect: 'postgres',
+            classMethods: classMethods
+        });
+
+        sequelize.options.define.classMethods.should.have.property('method1', classMethods.method1);
+        sequelize.options.define.classMethods.should.have.property('method2', classMethods.method2);
+        sequelize.options.define.classMethods.should.have.property('search', classMethods.search);
+    });
+
+    it('should allow to define `instanceMethods` options', function() {
+        var instanceMethods = {
+            method1: function() {},
+            method2: function() {},
+            normalize: function() {},
+        };
+
+        var sequelize = sequelizeBuilder({
+            host: 'localhost',
+            db: 'test',
+            username: 'root',
+            dialect: 'postgres',
+            instanceMethods: instanceMethods
+        });
+
+        sequelize.options.define.instanceMethods.should.have.property('method1', instanceMethods.method1);
+        sequelize.options.define.instanceMethods.should.have.property('method2', instanceMethods.method2);
+        sequelize.options.define.instanceMethods.should.have.property('normalize', instanceMethods.normalize);
     });
 });
