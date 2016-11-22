0bc42d7cd8e5af3a0959060ac34fa822995508f1
fogine <7884288+fogine@users.noreply.github.com>
2016-11-22 11:48:15 +0100

    cleanup

--- tests/unit/cli/commands/integrity.js
+++ tests/unit/cli/commands/integrity.js
@@ -3,7 +3,6 @@ var sinon          = require('sinon');
 var chai           = require('chai');
 var sinonChai      = require("sinon-chai");
 var chaiAsPromised = require('chai-as-promised');
-var Vantage        = require('vantage');
 var Promise        = require('bluebird');
 
 var AppManager       = require('../../../../lib/express/appManager.js');
@@ -36,7 +35,6 @@ describe('`integrity` command', function() {
                 value: {name: 'private'}
             }
         });
-        console.log(app);
 
         appManager.add(app);
         appManager.add(app2);
