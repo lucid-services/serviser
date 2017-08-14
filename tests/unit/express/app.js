3370e956c63c05e8253408374be60faf03590752
fogine <7884288+fogine@users.noreply.github.com>
2017-08-14 19:48:56 +0200

    use ajv validator instead of json-inspector

--- tests/unit/express/app.js
+++ tests/unit/express/app.js
@@ -97,8 +97,6 @@ describe('App', function() {
         beforeEach(function() {
             var app = this.app = this.appManager.buildApp(this.config, {name: '1'});
 
-            this.getExpressInjectorSpy = sinon.spy(ExpressValidator, 'getExpressInjector');
-
             this.preBuildSpy      = sinon.spy();
             this.postBuildSpy     = sinon.spy();
             this.statusChangedSpy = sinon.spy();
@@ -153,7 +151,6 @@ describe('App', function() {
             delete this.app;
 
             this.appEmitSpy.restore();
-            this.getExpressInjectorSpy.restore();
         });
 
         describe('on', function() {
@@ -227,15 +224,6 @@ describe('App', function() {
 
             it('should emit `pre-init` event', function() {
                 this.postInitSpy.should.have.been.calledOnce;
-            });
-
-            it('should pass CLONED options object to the json inspector injector middleware', function() {
-                var self = this;
-
-                this.getExpressInjectorSpy.should.have.been.calledOnce;
-                this.getExpressInjectorSpy.should.have.been.calledWith(sinon.match(function(options) {
-                    return self.app.options.validator !== options && options.should.be.eql(self.app.options.validator);
-                }));
             });
         });
 
