25fcb674eb5e3911a9d9d72dae495b4688fbfe7d
fogine <7884288+fogine@users.noreply.github.com>
2017-08-22 19:06:41 +0200

    adds bi-service shell executable (cli interface) + tests

---
+++ bin/bi-service.js
@@ -0,0 +1,253 @@
+#!/usr/bin/env node
+
+const yargs   = require('yargs');
+const cluster = require('cluster');
+const path    = require('path');
+const fs      = require('fs');
+const logger  = require('bi-logger');
+const _       = require('lodash');
+const json5   = require('json5');
+const config  = require('bi-config');
+
+const Service = require('../index.js');
+
+const CPU_COUNT = require('os').cpus().length;
+const VERSION   = require('../package.json').version;
+var _yargs = null; //yargs parser definition
+
+// adds .json5 loader require.extension
+require('json5/lib/require');
+
+//run only if this module isn't required by other node module
+if (module.parent === null) {
+
+    _yargs = yargs
+    .usage('$0 <command> [options]')
+    .command('*', '', {
+        'get-conf': {
+            alias: 'g',
+            describe: 'Prints resolved config value',
+            type: 'string'
+        },
+        json5: {
+            describe: 'if any json data are about to be printed they will be converted to json5 format',
+            type: 'boolean',
+            default: false
+        },
+        offset: {
+            describe: "A String or Number that's used to insert white space into the output JSON string for readability purposes.",
+            default: 4
+        }
+    }, defaultCmd)
+    .command(['run [options..]', 'start', 'serve'], 'Starts bi-service app - expects it to be located under cwd', {
+        cluster: {
+            alias: 'c',
+            describe: 'runs bi-service app in cluster mode - spawns child workers which are connected to the master process via IPC channel',
+            default: false,
+            type: 'boolean'
+        },
+        'parse-pos-args': {
+            describe: 'Whether to parse positional arguments',
+            type: 'boolean',
+            default: true
+        }
+    }, runCmd)
+    .option('config', {
+        describe: 'Custom config file destination',
+        global: true,
+        type: 'string'
+    })
+    .version('version', 'Prints bi-service version', VERSION);
+
+    try{
+        _yargs = require('bi-service-template')(_yargs);
+    } catch(e) {
+        if (e.code !== 'MODULE_NOT_FOUND') {
+            throw e;
+        }
+    }
+
+    const argv = _yargs
+        .help('h', false)
+        .alias('h', 'help')
+        .wrap(yargs.terminalWidth())
+        .argv;
+}
+
+module.exports.runCmd        = runCmd;
+module.exports.defaultCmd    = defaultCmd;
+module.exports._run          = _run;
+module.exports._runCluster   = _runCluster;
+module.exports._verifyCWD    = _verifyCWD;
+
+/**
+ * @param {Object} argv - shell arguments
+ */
+function runCmd(argv) {
+    var confOpt = _parseShellConfigOptions(argv);
+    config.initialize(confOpt);
+
+    if (argv.cluster) {
+        _runCluster();
+    } else {
+        _run();
+    }
+}
+
+/**
+ * @param {Object} argv - shell arguments
+ */
+function defaultCmd(argv) {
+    if (argv['get-conf'] !== undefined) {
+        config.initialize({fileConfigPath: argv.config});
+        var getOptionVal = argv['get-conf'];
+        var val;
+        if (!getOptionVal) {
+            val = config.get();
+        } else {
+            val = _.get(config.get(), getOptionVal);
+        }
+
+        if (val !== undefined) {
+            if (typeof val === 'object') {
+                var jsonUtils = argv.json5 ? json5 : JSON;
+
+                val = jsonUtils.stringify(val, null, argv.offset);
+            }
+            console.log(val);
+            process.exit();
+        } else {
+            console.error(val);
+            process.exit(1);
+        }
+    } else {
+        _yargs.showHelp();
+        process.exit(1);
+    }
+}
+
+/**
+ * returns parsed object with positional shell arguments.
+ * These options will then overwrite option values set in configuration file
+ * @param {Object} argv - shell arguments
+ * @return {Object}
+ */
+function _parseShellConfigOptions(argv) {
+    var out = {};
+
+    //`parse-pos-args` value is `true` by default, it must be
+    //explicitly set to falsy value thus undefined & null values does not count
+    if (argv['parse-pos-args'] === false || argv['parse-pos-args'] === 0) {
+        setConfigPathOption(out);
+        return out;
+    }
+
+    var options = argv.options.reduce(function(out, option, index) {
+        if (index % 2 === 0) {
+            out.names.push(option);
+        } else {
+            out.values.push(option);
+        }
+        return out;
+    }, {
+        names: [],
+        values: []
+    });
+
+    if (argv.options.length % 2 !== 0) {
+        throw new Error(
+            `Invalid number of shell positional arguments received.
+            Possitional arguments are expected to be in "[key] [value]" pairs`
+        );
+    }
+
+    options.names.forEach(function(propPath, index) {
+        _.set(
+            out,
+            propPath,
+            json5.parse(options.values[index])
+        );
+    });
+
+    setConfigPathOption(out);
+
+    return out;
+
+    function setConfigPathOption(obj) {
+        //for overwriting expected config filepath we can use --config option only
+        if (argv.config) {
+            obj.fileConfigPath = argv.config;
+            obj.fileConfigPath = path.normalize(obj.fileConfigPath);
+        } else {
+            delete obj.fileConfigPath;
+        }
+    }
+}
+
+/**
+ *
+ */
+function _verifyCWD() {
+
+    packagePath = process.cwd() + '/package.json';
+
+    try {
+        require(packagePath);
+    } catch(e) {
+        if (e.code !== 'MODULE_NOT_FOUND') {
+            throw e;
+        }
+        console.error(`Could not confirm that cwd is a bi-service project:`);
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+/**
+ * searches for custom bin/www startup script if not found, it starts up
+ * the service itself.
+ * @return {undefined}
+ */
+function _run() {
+    _verifyCWD();
+
+    var customStartupFilePath = path.resolve(process.cwd() + '/bin/www');
+    var projectIndex = path.resolve(process.cwd() + '/index.js');
+
+    if (fs.existsSync(customStartupFilePath)) {
+        require(customStartupFilePath);
+    } else if (fs.existsSync(projectIndex)) {
+        let service = require(projectIndex);
+        if (!(service instanceof Service)) {
+            console.error(`${projectIndex} does not export a Service object`);
+            process.exit(1);
+        }
+
+        service.listen();
+    }
+}
+
+/**
+ * @return {undefined}
+ */
+function _runCluster() {
+    if (cluster.isMaster) {
+        _verifyCWD();
+        // Create a worker for each CPU
+        for (let i = 0; i < CPU_COUNT; i += 1) {
+            cluster.fork();
+        }
+        // Listen for dying workers
+        cluster.on('exit', function () {
+            setTimeout(function() {
+                cluster.fork();
+            }, 5000);
+        });
+        //
+        cluster.on('disconnect', function(worker) {
+            console.error('disconnect!');
+        });
+    } else {
+        _run();
+    }
+}
---
+++ tests/acceptance/bin/bi-service.js
@@ -0,0 +1,335 @@
+const _              = require('lodash');
+const findPorts      = require('find-port');
+const tmp            = require('tmp');
+const json5          = require('json5');
+const fs             = require('fs');
+const path           = require('path');
+const sinon          = require('sinon');
+const chai           = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai      = require("sinon-chai");
+const Promise        = require('bluebird');
+const spawn          = require('child_process').spawn;
+
+const biServicePath = path.resolve(__dirname + '/../../../bin/bi-service.js');
+const MOCK_APP_ROOT = path.resolve(__dirname + '/../../mocks/app');
+const MOCK_APP_CONFIG_PATH = path.resolve(__dirname + '/../../mocks/app/config.json5');
+const BI_SERVICE_VERSION = require('../../../package.json').version;
+
+//this makes sinon-as-promised available in sinon:
+require('sinon-as-promised', Promise);
+
+// adds .json5 loader require.extension
+require('json5/lib/require');
+
+const expect = chai.expect;
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('bin/bi-service', function() {
+    before(function() {
+        var self = this;
+        this.spawn = _spawn;
+        this.portAvailable = function(port) {
+            return new Promise(function(resolve, reject) {
+                findPorts('127.0.0.1', [port], function(ports) {
+                    ports = ports || [];
+
+                    if (~ports.indexOf(port)) {
+                        return resolve(true);
+                    }
+                    return resolve(false);
+                });
+            });
+        };
+
+        /**
+         * @param {Array}   args
+         * @param {Object}  [options]
+         * @param {Integer} [options.port]
+         * @param {String}  [options.cwd]
+         */
+        function _spawn(args, options) {
+            options = options || {};
+            args = args || [];
+            args.unshift(biServicePath);
+
+            return new Promise(function(resolve, reject) {
+                var stdout = ''
+                ,   stderr = ''
+                ,   port   = options.port
+                ,   intervalID = null;
+
+                if (port === undefined && ~args.indexOf('apps.app1.listen')) {
+                    port = args[args.indexOf('apps.app1.listen') + 1];
+                }
+
+                if (typeof port === 'number') {
+                    intervalID = setInterval(killOnTCPUnavailable, 125);
+                }
+
+                var proc = spawn('node', args, {
+                    cwd: options.cwd || MOCK_APP_ROOT
+                });
+
+                proc.stdout.on('data', function (data) {
+                    stdout += data.toString().trim();
+                });
+
+                proc.stderr.on('data', function (data) {
+                    stderr += data.toString().trim();
+                });
+
+                proc.on('exit', function (code) {
+                    if (code !== 0 && code !== null) {
+                        let err = new Error(`${stderr}\nprocess exited with code: ${code}`);
+                        err.code = code;
+                        err.stderr = stderr;
+                        return reject(err);
+                    }
+                    return resolve({
+                        stdout: stdout,
+                        code: code
+                    });
+                });
+
+                function killOnTCPUnavailable() {
+                    self.portAvailable(port).then(function(available) {
+                        if (!available) {
+                            clearInterval(intervalID);
+                            proc.stdin.pause();
+                            proc.kill();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    describe('run/server/start service', function() {
+        before(function(done) {
+            var self = this;
+
+            tmp.setGracefulCleanup();
+            this.tmpDir = tmp.dirSync({unsafeCleanup: true});
+
+            findPorts('127.0.0.1', 8000, 8010, function(ports) {
+                self.port = ports.shift();
+
+                var conf = require(MOCK_APP_CONFIG_PATH);
+                conf.apps.app1.listen = self.port;
+
+                fs.writeFile(
+                    `${self.tmpDir.name}/config.json5`,
+                    JSON.stringify(conf),
+                    function(err) {
+                        if (err) {
+                            return done(err);
+                        }
+                        done();
+                    });
+            });
+        });
+
+        it('should start a nodejs server with given port', function() {
+            return this.spawn([
+                'run',
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                'apps.app1.listen',
+                this.port
+            ]).should.be.fulfilled;
+        });
+
+        it('should exit with 1 when the start command is run outside of project directory (invalid project)', function() {
+            return this.spawn([
+                'run',
+                '--config',
+                MOCK_APP_CONFIG_PATH
+            ], {
+                cwd: __dirname
+            }).should.be.rejected.then(function(err) {
+                err.stderr.should.match(/Could not confirm that cwd is a bi-service project/);
+            });
+        });
+
+        it('should start a nodejs server in cluster mode', function() {
+            return this.spawn([
+                'run',
+                '--cluster',
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                'apps.app1.listen',
+                this.port
+            ]).should.be.fulfilled;
+        });
+
+        describe('--parse-pos-args', function() {
+            describe('when disabled', function() {
+                it('should ignore shell positional arguments (which would otherwise overwrite config file option values)', function() {
+                    return this.spawn([
+                        'run',
+                        '--config',
+                        `${this.tmpDir.name}/config.json5`,
+                        '--parse-pos-args',
+                        false,
+                        'apps.app1.listen',
+                        'invalid'
+                    ], {port: this.port}).should.be.fulfilled;
+                });
+            });
+
+            describe('when enabled', function() {
+                it('should exit with 1 when we provide invalid number of positional args', function() {
+                    return this.spawn([
+                        'run',
+                        '--config',
+                        MOCK_APP_CONFIG_PATH,
+                        'couchbase.host',
+                        '"127.0.0.3"',
+                        'another.config.option.with.no.value'
+                    ]).should.be.rejected.then(function(err) {
+                        err.code.should.be.equal(1);
+                    });
+                });
+            });
+        });
+    });
+
+    describe('--version', function() {
+        it('should dump bi-service version tag to stdout', function() {
+            return this.spawn([
+                '--version',
+            ]).should.be.fulfilled.then(function(result) {
+                result.stdout.should.be.equal(BI_SERVICE_VERSION);
+                result.code.should.be.equal(0);
+            });
+        });
+    });
+
+    describe('--get-conf', function() {
+        it('should print option value', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+                'apps.app1.bodyParser.json.type'
+            ]).should.be.fulfilled.then(function(result) {
+                result.code.should.be.equal(0);
+                result.stdout.should.be.eql('application/json');
+            });
+        });
+
+        it('should print option value', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+            ]).should.be.fulfilled.then(function(result) {
+                result.code.should.be.equal(0);
+                json5.parse(result.stdout).should.be.eql({
+                    apps: {
+                        app1: {
+                            baseUrl: '127.0.0.1',
+                            listen: 5903,
+                            bodyParser: {
+                                json: {
+                                    extended: true,
+                                    type: 'application/json',
+                                    limit: "2mb"
+                                }
+                            },
+                        }
+                    },
+                    fileConfigPath: MOCK_APP_CONFIG_PATH,
+                    type: 'literal'
+                });
+            });
+        });
+
+        it('should exit with 1 and print "undefined" when there is not such option', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+                'some.options.which.does.not.exist'
+            ]).should.be.rejected.then(function(result) {
+                result.code.should.be.equal(1);
+                result.stderr.should.be.equal('undefined');
+            });
+        });
+    });
+
+    describe('--json5 option', function() {
+        it('should print json data in json5 format', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+                'apps.app1.bodyParser',
+                '--json5'
+            ]).should.be.fulfilled.then(function(result) {
+                var stdout = result.stdout;
+                result.code.should.be.equal(0);
+
+                stdout.should.be.equal('{\n'          +
+                '    json: {\n'                       +
+                '        extended: true,\n'           +
+                '        type: "application/json",\n' +
+                '        limit: "2mb"\n'              +
+                '    }\n'                             +
+                '}');
+            });
+        });
+    });
+
+    describe('--offset option', function() {
+        it('should print json data with correct space offset set', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+                'apps.app1.bodyParser',
+                '--json5',
+                '--offset',
+                '2'
+            ]).should.be.fulfilled.then(function(result) {
+                var stdout = result.stdout;
+                result.code.should.be.equal(0);
+
+                stdout.should.be.equal('{\n'      +
+                '  json: {\n'                     +
+                '    extended: true,\n'           +
+                '    type: "application/json",\n' +
+                '    limit: "2mb"\n'              +
+                '  }\n'                           +
+                '}');
+            });
+        });
+
+        it('should replace space character with given string value in JSON output', function() {
+            return this.spawn([
+                '--config',
+                MOCK_APP_CONFIG_PATH,
+                '--get-conf',
+                'apps.app1.bodyParser',
+                '--json5',
+                '--offset',
+                '__'
+            ]).should.be.fulfilled.then(function(result) {
+                var stdout = result.stdout;
+                result.code.should.be.equal(0);
+
+                stdout.should.be.equal('{\n'      +
+                '__json: {\n'                     +
+                '____extended: true,\n'           +
+                '____type: "application/json",\n' +
+                '____limit: "2mb"\n'              +
+                '__}\n'                           +
+                '}');
+            });
+        });
+    });
+});
