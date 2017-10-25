26d829b07ef3815604fa71789431ce50c998b3b7
fogine <7884288+fogine@users.noreply.github.com>
2017-10-25 19:38:43 +0200

    documentation

--- index.js
+++ index.js
@@ -7,27 +7,100 @@ exports = module.exports = require('./lib/service.js');
 
 exports.Express              = require('express');
 exports.EXPRESS_VERSION      = parseInt(require('express/package.json').version[0]);
-exports.VERSION              = require('./package.json').version;
-exports.AppManager           = require('./lib/express/appManager.js');
-exports.App                  = require('./lib/express/app.js');
-exports.Service              = require('./lib/service.js');
-exports.ResourceManager      = require('./lib/resourceManager.js');
+/**
+ * `bi-service` npm package version
+ * @name Service.VERSION
+ * @type {String}
+ */
+exports.VERSION = require('./package.json').version;
+/**
+ * @name Service.AppManager
+ * @type {AppManager}
+ */
+exports.AppManager = require('./lib/express/appManager.js');
+/**
+ * @name Service.App
+ * @type {App}
+ */
+exports.App = require('./lib/express/app.js');
+/**
+ * self reference
+ * @name Service.Service
+ * @type {Service}
+ */
+exports.Service = require('./lib/service.js');
+/**
+ * @name Service.ResourceManager
+ * @type {ResourceManager}
+ */
+exports.ResourceManager = require('./lib/resourceManager.js');
+/**
+ * @name Service.RemoteServiceManager
+ * @type {RemoteServiceManager}
+ */
 exports.RemoteServiceManager = require('./lib/remoteServiceManager.js');
-exports.AppStatus            = require('./lib/express/appStatus.js');
-exports.Response             = require('./lib/express/response.js');
-exports.Router               = require('./lib/express/router.js');
-exports.Route                = require('./lib/express/route.js');
-exports.RequestType          = require('./lib/express/requestType.js');
-exports.moduleLoader         = require('./lib/moduleLoader.js');
-exports.utils                = require('./lib/utils.js');
+/**
+ * @name Service.AppStatus
+ * @type {AppStatus}
+ */
+exports.AppStatus = require('./lib/express/appStatus.js');
+/**
+ * @name Service.Response
+ * @type {Response}
+ */
+exports.Response = require('./lib/express/response.js');
+/**
+ * @name Service.Router
+ * @type {Router}
+ */
+exports.Router = require('./lib/express/router.js');
+/**
+ * @name Service.Route
+ * @type {Route}
+ */
+exports.Route = require('./lib/express/route.js');
+/**
+ * @name Service.RequestType
+ * @type {RequestType}
+ */
+exports.RequestType = require('./lib/express/requestType.js');
+/**
+ * @name Service.moduleLoader
+ * @type {ModuleLoader}
+ */
+exports.moduleLoader = require('./lib/moduleLoader.js');
+/**
+ * @name Service.utils
+ * @type {Utils}
+ */
+exports.utils = require('./lib/utils.js');
 
-exports.error       = {
+
+
+/**
+ * @name Service.error
+ * @type {ErrorList}
+ */
+exports.error = {
     RouteError          : require('./lib/error/routeError.js'),
+    RouterError         : require('./lib/error/routerError.js'),
     RouteNotFoundError  : require('./lib/error/routeNotFoundError.js'),
     RequestError        : require('./lib/error/requestError.js'),
-    RouteError          : require('./lib/error/routeError.js'),
     ServiceError        : require('./lib/error/serviceError.js'),
     ValidationError     : require('./lib/error/validationError.js'),
     ForbiddenError      : require('./lib/error/forbiddenError.js'),
     UnauthorizedError   : require('./lib/error/unauthorizedError.js')
 };
+
+/**
+ * @typedef ErrorList
+ * @type {Object}
+ * @property {RouteError}         RouteError
+ * @property {RouteNotFoundError} RouteNotFoundError
+ * @property {RequestError}       RequestError
+ * @property {RouteError}         RouteError
+ * @property {ServiceError}       ServiceError
+ * @property {ValidationError}    ValidationError
+ * @property {ForbiddenError}     ForbiddenError
+ * @property {UnauthorizedError}  UnauthorizedError
+ */
