98993409f46e1a8c47ef0d6567204dcbf0552586
fogine <7884288+fogine@users.noreply.github.com>
2018-01-02 17:38:24 +0100

    docs

--- index.js
+++ index.js
@@ -11,73 +11,87 @@ exports.EXPRESS_VERSION      = parseInt(require('express/package.json').version[
  * `bi-service` npm package version
  * @name Service.VERSION
  * @type {String}
+ * @readonly
  */
 exports.VERSION = require('./package.json').version;
 /**
  * @name Service.AppManager
  * @type {AppManager}
+ * @readonly
  */
 exports.AppManager = require('./lib/appManager.js');
 /**
  * @name Service.App
  * @type {App}
+ * @readonly
  */
 exports.App = require('./lib/express/app.js');
 /**
  * self reference
  * @name Service.Service
  * @type {Service}
+ * @readonly
  */
 exports.Service = require('./lib/service.js');
 /**
  * @name Service.ResourceManager
  * @type {ResourceManager}
+ * @readonly
  */
 exports.ResourceManager = require('./lib/resourceManager.js');
 /**
  * @name Service.RemoteServiceManager
  * @type {RemoteServiceManager}
+ * @readonly
  */
 exports.RemoteServiceManager = require('./lib/remoteServiceManager.js');
 /**
  * @name Service.AppStatus
  * @type {AppStatus}
+ * @readonly
  */
 exports.AppStatus = require('./lib/common/appStatus.js');
 /**
  * @name Service.Response
  * @type {Response}
+ * @readonly
  */
 exports.Response = require('./lib/response.js');
 /**
  * @name Service.Router
  * @type {Router}
+ * @readonly
  */
 exports.Router = require('./lib/express/router.js');
 /**
  * @name Service.Route
  * @type {Route}
+ * @readonly
  */
 exports.Route = require('./lib/express/route.js');
 /**
  * @name Service.RequestType
  * @type {RequestType}
+ * @readonly
  */
 exports.RequestType = require('./lib/express/requestType.js');
 /**
  * @name Service.moduleLoader
  * @type {ModuleLoader}
+ * @readonly
  */
 exports.moduleLoader = require('./lib/moduleLoader.js');
 /**
  * @name Service.utils
  * @type {Utils}
+ * @readonly
  */
 exports.utils = require('./lib/utils.js');
 
 /**
  * @name Service.common
  * @type {CommonList}
+ * @readonly
  */
 exports.common = {
     App: require('./lib/common/app.js'),
@@ -96,6 +110,7 @@ exports.common = {
 /**
  * @name Service.error
  * @type {ErrorList}
+ * @readonly
  */
 exports.error = {
     RouteError          : require('./lib/error/routeError.js'),
@@ -114,7 +129,6 @@ exports.error = {
  * @property {RouteError}         RouteError
  * @property {RouteNotFoundError} RouteNotFoundError
  * @property {RequestError}       RequestError
- * @property {RouteError}         RouteError
  * @property {ServiceError}       ServiceError
  * @property {ValidationError}    ValidationError
  * @property {ForbiddenError}     ForbiddenError
